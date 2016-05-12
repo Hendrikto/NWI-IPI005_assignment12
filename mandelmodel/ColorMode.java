c70d7dee6adbca96640a85380339ac39c52c24ba
Hendrik Werner <hendrik.to@gmail.com>
2016-05-12 23:01:07 +0200

    Remove a lot of code by choosing better methods.

--- mandelmodel/ColorMode.java
+++ mandelmodel/ColorMode.java
@@ -17,38 +17,28 @@ public enum ColorMode {
     Random {
         @Override
         public Color getColor(int i, int max) {
-            return Color.rgb(randomRGB(), randomRGB(), randomRGB());
-        }
-
-        private int randomRGB() {
-            return (int) (Math.random() * MAX_RGB);
+            return Color.color(Math.random(), Math.random(), Math.random());
         }
     },
     Red {
         @Override
         public Color getColor(int i, int max) {
-            return Color.rgb(fraction(i, max), 0, 0);
+            return Color.color((double) i / max, 0, 0);
         }
     },
     Green {
         @Override
         public Color getColor(int i, int max) {
-            return Color.rgb(0, fraction(i, max), 0);
+            return Color.color(0, (double) i / max, 0);
         }
     },
     Blue {
         @Override
         public Color getColor(int i, int max) {
-            return Color.rgb(0, 0, fraction(i, max));
+            return Color.color(0, 0, (double) i / max);
         }
     };
 
-    private static final int MAX_RGB = 255;
-
     public abstract Color getColor(int i, int max);
-
-    private static int fraction(int i, int max) {
-        return (int) ((double) i / max * MAX_RGB);
-    }
 
 }
