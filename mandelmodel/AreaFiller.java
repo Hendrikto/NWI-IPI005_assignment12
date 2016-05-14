dc5d4409ff7f0da5f79301f6800c6097108f98ed
Hendrik Werner <hendrik.to@gmail.com>
2016-05-14 17:09:03 +0200

    Separate concerns.

--- mandelmodel/AreaFiller.java
+++ mandelmodel/AreaFiller.java
@@ -13,47 +13,53 @@ import javafx.scene.image.PixelWriter;
 public class AreaFiller {
 
     private int iterations;
+    private int scale;
+    private int size;
+    private double startX;
+    private double startY;
     private ColorMap colorMap;
+    private MandelbrotMap mandelMap;
 
     /**
+     * @param size the size of the area to fill
+     * @param startX the x value of the upper left corner
+     * @param startY the y value of the upper left corner
+     * @param scale the scaling factor
      * @param iterations the maximum number of iterations per point
      * @param mode the color mode
      */
-    public AreaFiller(int iterations, ColorMode mode) {
+    public AreaFiller(int size, double startX, double startY, int scale, int iterations, ColorMode mode) {
+        this.size = size;
+        this.startX = startX;
+        this.startY = startY;
+        this.scale = scale;
         this.iterations = iterations;
         colorMap = new ColorMap(iterations, mode);
+        mandelMap = new MandelbrotMap(size, startX, startY, scale, iterations);
     }
 
     /**
-     * @param iterations the maximum number of iterations per point
+     * @param size the size of the area to fill
+     * @param startX the x value of the upper left corner
+     * @param startY the y value of the upper left corner
+     * @param scale the scaling factor
      */
-    public AreaFiller(int iterations) {
-        this(iterations, ColorMode.ColorfulInverted);
-    }
-
-    /**
-     * Standard constructor.
-     */
-    public AreaFiller() {
-        this(20);
+    public AreaFiller(int size, double startX, double startY, int scale) {
+        this(size, startX, startY, scale, 20, ColorMode.ColorfulInverted);
     }
 
     /**
      * Fill the canvas with some arbitrarily chosen pattern.
      *
      * @param canvas the canvas to fill
-     * @param x staring position on the x-axis
-     * @param y starting position on the y-axis
-     * @param scale the scaling factor
      */
-    public void fill(Canvas canvas, double x, double y, int scale) {
+    public void fill(Canvas canvas) {
         int imageWidth = (int) canvas.getWidth();
         int imageHeight = (int) canvas.getHeight();
         final PixelWriter pixelWriter = canvas.getGraphicsContext2D().getPixelWriter();
-        MandelbrotMap map = new MandelbrotMap(imageWidth, x, y, scale, iterations);
         for (int i = 0; i < imageWidth; i++) {
             for (int j = 0; j < imageHeight; j++) {
-                pixelWriter.setColor(i, j, colorMap.getColor(map.getValue(i, j)));
+                pixelWriter.setColor(i, j, colorMap.getColor(mandelMap.getValue(i, j)));
             }
         }
     }
