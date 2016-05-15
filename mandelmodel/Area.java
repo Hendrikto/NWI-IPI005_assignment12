92e99f680e8218d8481b7b35eeb62c89c4f28acf
Hendrik Werner <hendrik.to@gmail.com>
2016-05-15 14:16:22 +0200

    Add a class to represent a two dimensional area.

---
+++ mandelmodel/Area.java
@@ -0,0 +1,28 @@
+package mandelmodel;
+
+/**
+ * A two dimensional area.
+ *
+ * @author Hendrik Werner // s4549775
+ */
+public class Area {
+
+    public final double upperLeftX;
+    public final double upperLeftY;
+    public final double width;
+    public final double height;
+
+    /**
+     * @param upperLeftX the x value of the upper left corner of the area
+     * @param upperLeftY the y value of the upper left corner of the area
+     * @param width the width of the area
+     * @param height the height of the area
+     */
+    public Area(double upperLeftX, double upperLeftY, double width, double height) {
+        this.upperLeftX = upperLeftX;
+        this.upperLeftY = upperLeftY;
+        this.width = width;
+        this.height = height;
+    }
+
+}
