cd9c2c02e9c3665755edcc1a30b7312f1944df8f
Hendrik Werner <hendrik.to@gmail.com>
2016-07-29 00:47:42 +0200

    Finish refactor to FXML
    * Rewire all event handlers.
    * Increase canvas size to the original 1000x1000px.
    * Remove old UI generation code.

--- mandelfx/MandelFX.java
+++ mandelfx/MandelFX.java
@@ -2,29 +2,10 @@ package mandelfx;
 
 import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import mandelmodel.Area;
-import mandelmodel.AreaFiller;
-import mandelmodel.ColorMode;
 
 /**
  *
@@ -33,30 +14,6 @@ import mandelmodel.ColorMode;
  * @author Sjaak Smetsers
  */
 public class MandelFX extends Application {
-
-    private static final int GRID_SIZE = 1000;
-    private static final int SPACING = 10;
-    private static final double ZOOM_IN = 2;
-    private static final double ZOOM_OUT = .5;
-
-    private AreaFiller filler;
-    private boolean mouseDragged;
-    private double startX;
-    private double startY;
-
-    /**
-     * JavaFX
-     */
-    private Group canvasContainer;
-    private Canvas canvas;
-    private Rectangle selection;
-    private TextField inputCenterX;
-    private TextField inputCenterY;
-    private TextField inputSize;
-    private TextField inputIterations;
-    private Button btnDraw;
-    private Button btnUpdateIterations;
-    private ChoiceBox<ColorMode> colorChoice;
 
     /**
      * Start the app.
@@ -76,179 +33,6 @@ public class MandelFX extends Application {
      */
     public static void main(String[] args) {
         launch(args);
-    }
-
-    /**
-     * Create the scene.
-     *
-     * @return the scene
-     */
-    private Scene makeScene() {
-        canvas = new Canvas(GRID_SIZE, GRID_SIZE);
-        canvas.setOnMousePressed(this::handleMousePressed);
-        canvas.setOnMouseReleased(this::handleMouseReleased);
-        filler = new AreaFiller(new Area(-2, -2, 4, 4), GRID_SIZE);
-        filler.fill(canvas);
-        selection = new Rectangle();
-        selection.setStroke(Color.GREENYELLOW);
-        selection.setFill(Color.TRANSPARENT);
-        canvasContainer = new Group(canvas);
-        canvasContainer.setOnMouseDragged(this::handleMouseDragged);
-        canvasContainer.getChildren().add(selection);
-        HBox root = new HBox(canvasContainer, makeControls());
-        Scene scene = new Scene(root);
-        return scene;
-    }
-
-    /**
-     * @return the control panel
-     */
-    private Pane makeControls() {
-        instantiateControls();
-        Label areaLabel = new Label("Area");
-        areaLabel.setFont(Font.font(20));
-        Label detailsLabel = new Label("Details");
-        detailsLabel.setFont(Font.font(20));
-        VBox controls = new VBox(
-                areaLabel,
-                makeAreaControls(),
-                detailsLabel,
-                makeDetailControls()
-        );
-        controls.setPadding(new Insets(SPACING));
-        controls.setSpacing(SPACING * 2);
-        return controls;
-    }
-
-    /**
-     * @return the area controls
-     */
-    private Pane makeAreaControls() {
-        GridPane grid = new GridPane();
-        grid.setHgap(SPACING);
-        grid.setVgap(SPACING);
-        grid.add(new Label("center x:"), 0, 0);
-        grid.add(inputCenterX, 1, 0);
-        grid.add(new Label("center y:"), 0, 1);
-        grid.add(inputCenterY, 1, 1);
-        grid.add(new Label("size:"), 0, 2);
-        grid.add(inputSize, 1, 2);
-        grid.add(btnDraw, 1, 3);
-        return grid;
-    }
-
-    /**
-     * @return the detail controls
-     */
-    private Pane makeDetailControls() {
-        GridPane grid = new GridPane();
-        grid.setHgap(SPACING);
-        grid.setVgap(SPACING);
-        grid.add(new Label("Iterations:"), 0, 0);
-        grid.add(inputIterations, 1, 0);
-        grid.add(btnUpdateIterations, 2, 0);
-        grid.add(new Label("Color:"), 0, 1);
-        grid.add(colorChoice, 1, 1);
-        return grid;
-    }
-
-    /**
-     * Instantiate control elements.
-     */
-    private void instantiateControls() {
-        inputCenterX = new TextField("0");
-        inputCenterY = new TextField("0");
-        inputSize = new TextField("4");
-        inputIterations = new TextField("20");
-        btnDraw = new Button("Draw");
-        btnDraw.setOnAction(this::handleDrawButtonAction);
-        btnUpdateIterations = new Button("Update");
-        btnUpdateIterations.setOnAction(this::handleUpdateIterations);
-        colorChoice = new ChoiceBox<>();
-        colorChoice.getItems().addAll(ColorMode.values());
-        colorChoice.setValue(ColorMode.ColorfulInverted);
-        colorChoice.setOnAction(this::handleUpdateColorMode);
-    }
-
-    /**
-     * Handle a mouse button press.
-     *
-     * @param e the mouse event
-     */
-    private void handleMousePressed(MouseEvent e) {
-        startX = e.getX();
-        startY = e.getY();
-    }
-
-    /**
-     * Handle a mouse button release.
-     *
-     * @param e the mouse event
-     */
-    private void handleMouseReleased(MouseEvent e) {
-        if (mouseDragged) {
-            filler.zoom(
-                    Math.min(startX, e.getX()),
-                    Math.min(startY, e.getY()),
-                    Math.abs(startX - e.getX()),
-                    Math.abs(startY - e.getY())
-            );
-        } else {
-            filler.zoom(e.getX(), e.getY(), e.isShiftDown() ? ZOOM_OUT : ZOOM_IN);
-        }
-        mouseDragged = false;
-        filler.fill(canvas);
-        selection.setVisible(false);
-    }
-
-    /**
-     * Handle dragging the mouse.
-     *
-     * @param e the mouse event
-     */
-    private void handleMouseDragged(MouseEvent e) {
-        mouseDragged = true;
-        selection.setVisible(true);
-        selection.setX(Math.min(startX, e.getX()));
-        selection.setY(Math.min(startY, e.getY()));
-        selection.setWidth(Math.abs(startX - e.getX()));
-        selection.setHeight(Math.abs(startY - e.getY()));
-    }
-
-    /**
-     * Handle the draw button.
-     *
-     * @param e the action event
-     */
-    private void handleDrawButtonAction(ActionEvent e) {
-        double size = Double.parseDouble(inputSize.getText());
-        filler.setArea(new Area(
-                Double.parseDouble(inputCenterX.getText()) - size / 2,
-                Double.parseDouble(inputCenterY.getText()) - size / 2,
-                size,
-                size
-        ));
-        filler.fill(canvas);
-    }
-
-    /**
-     * Update the iterations with data from the ui.
-     *
-     * @param e the action event
-     */
-    private void handleUpdateIterations(ActionEvent e) {
-        filler.setIterations(Integer.parseInt(inputIterations.getText()));
-        filler.fill(canvas);
-    }
-
-    /**
-     * Update the color mode with data from the ui.
-     *
-     * @param e the action event
-     */
-    private void handleUpdateColorMode(ActionEvent e) {
-        filler.setColorMode(colorChoice.getValue());
-        filler.fill(canvas);
     }
 
 }
--- mandelfx/UIController.java
+++ mandelfx/UIController.java
@@ -2,11 +2,15 @@ package mandelfx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
 import mandelmodel.Area;
 import mandelmodel.AreaFiller;
 import mandelmodel.ColorMode;
@@ -18,13 +22,22 @@ import mandelmodel.ColorMode;
  */
 public class UIController implements Initializable {
 
+    private static final double ZOOM_IN = 2;
+    private static final double ZOOM_OUT = .5;
+
     @FXML Canvas canvas;
+    @FXML Rectangle selection;
     @FXML TextField xInput;
     @FXML TextField yInput;
     @FXML TextField sizeInput;
+    @FXML TextField iterationsInput;
+    @FXML Button drawButton;
     @FXML ChoiceBox<ColorMode> colorChoice;
 
     private AreaFiller filler;
+    private boolean mouseDragged;
+    private double startX;
+    private double startY;
 
     /**
      * Initializes the controller class.
@@ -32,7 +45,96 @@ public class UIController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         colorChoice.getItems().addAll(ColorMode.values());
+        colorChoice.setValue(ColorMode.ColorfulInverted);
+        colorChoice.setOnAction(this::handleUpdateColorMode);
         filler = new AreaFiller(new Area(-2, -2, 4, 4), (int) canvas.getWidth());
+        filler.fill(canvas);
+    }
+
+    /**
+     * Handle a mouse button press.
+     *
+     * @param e the mouse event
+     */
+    @FXML
+    private void handleMousePressed(MouseEvent e) {
+        startX = e.getX();
+        startY = e.getY();
+    }
+
+    /**
+     * Handle a mouse button release.
+     *
+     * @param e the mouse event
+     */
+    @FXML
+    private void handleMouseReleased(MouseEvent e) {
+        if (mouseDragged) {
+            filler.zoom(
+                    Math.min(startX, e.getX()),
+                    Math.min(startY, e.getY()),
+                    Math.abs(startX - e.getX()),
+                    Math.abs(startY - e.getY())
+            );
+        } else {
+            filler.zoom(e.getX(), e.getY(), e.isShiftDown() ? ZOOM_OUT : ZOOM_IN);
+        }
+        mouseDragged = false;
+        filler.fill(canvas);
+        selection.setVisible(false);
+    }
+
+    /**
+     * Handle dragging the mouse.
+     *
+     * @param e the mouse event
+     */
+    @FXML
+    private void handleMouseDragged(MouseEvent e) {
+        mouseDragged = true;
+        selection.setVisible(true);
+        selection.setX(Math.min(startX, e.getX()));
+        selection.setY(Math.min(startY, e.getY()));
+        selection.setWidth(Math.abs(startX - e.getX()));
+        selection.setHeight(Math.abs(startY - e.getY()));
+    }
+
+    /**
+     * Handle the draw button.
+     *
+     * @param e the action event
+     */
+    @FXML
+    private void handleDraw(ActionEvent e) {
+        double size = Double.parseDouble(sizeInput.getText());
+        filler.setArea(new Area(
+                Double.parseDouble(xInput.getText()) - size / 2,
+                Double.parseDouble(yInput.getText()) - size / 2,
+                size,
+                size
+        ));
+        filler.fill(canvas);
+    }
+
+    /**
+     * Update the iterations with data from the ui.
+     *
+     * @param e the action event
+     */
+    @FXML
+    private void handleUpdateIterations(ActionEvent e) {
+        filler.setIterations(Integer.parseInt(iterationsInput.getText()));
+        filler.fill(canvas);
+    }
+
+    /**
+     * Update the color mode with data from the ui.
+     *
+     * @param e the action event
+     */
+    @FXML
+    private void handleUpdateColorMode(ActionEvent e) {
+        filler.setColorMode(colorChoice.getValue());
         filler.fill(canvas);
     }
 
