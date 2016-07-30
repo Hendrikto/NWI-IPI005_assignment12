7cc725cbd9a06cc53471b93295845a47c368ece1
Hendrik Werner <hendrik.to@gmail.com>
2016-07-30 18:29:44 +0200

    Convert class to JavaFX bean.

--- mandelmodel/Area.java
+++ mandelmodel/Area.java
@@ -1,29 +1,59 @@
 package mandelmodel;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
 /**
  * A two dimensional area.
  *
  * @author Hendrik Werner // s4549775
- * @author Jasper Haasdijk // s4449754
  */
 public class Area {
 
-    public final double upperLeftX;
-    public final double upperLeftY;
-    public final double width;
-    public final double height;
+    private final DoubleProperty upperLeftX;
+    private final DoubleProperty upperLeftY;
+    private final DoubleProperty width;
+    private final DoubleProperty height;
 
     /**
-     * @param upperLeftX the x value of the upper left corner of the area
-     * @param upperLeftY the y value of the upper left corner of the area
-     * @param width the width of the area
-     * @param height the height of the area
+     * @param upperLeftX the initial x value of the upper left corner of the area
+     * @param upperLeftY the initial y value of the upper left corner of the area
+     * @param width the initial width of the area
+     * @param height the initial height of the area
      */
     public Area(double upperLeftX, double upperLeftY, double width, double height) {
-        this.upperLeftX = upperLeftX;
-        this.upperLeftY = upperLeftY;
-        this.width = width;
-        this.height = height;
+        this.upperLeftX = new SimpleDoubleProperty(upperLeftX);
+        this.upperLeftY = new SimpleDoubleProperty(upperLeftY);
+        this.width = new SimpleDoubleProperty(width);
+        this.height = new SimpleDoubleProperty(height);
+    }
+
+    /**
+     * @return the upper left corner on the x-axis
+     */
+    public DoubleProperty upperLeftXProperty() {
+        return upperLeftX;
+    }
+
+    /**
+     * @return the upper left corner on the y-axis
+     */
+    public DoubleProperty upperLeftYProperty() {
+        return upperLeftY;
+    }
+
+    /**
+     * @return the width
+     */
+    public DoubleProperty widthProperty() {
+        return width;
+    }
+
+    /**
+     * @return the height
+     */
+    public DoubleProperty heightProperty() {
+        return height;
     }
 
 }
